6549c5b8a303a608a21e32fa6be51c5e26355f8c
Simon Cole <simonccole1@gmail.com>
2023-07-03 12:34:32 -0400

    added capitalizing fn also from IGV.js

--- stringUtils.js
+++ stringUtils.js
@@ -1,2 +1,6 @@
 export const ignoreCaseIndexOf = (arr, lookup) => arr.findIndex(item => 
-lookup.toLowerCase() === item.toLowerCase());% 
+lookup.toLowerCase() === item.toLowerCase());
+export const capitalize = (str) => {
+    return str.length > 0 ? str.charAt(0).toUpperCase() + str.slice(1) : 
+str;
+}% 
