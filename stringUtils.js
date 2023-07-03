d18374c1bf55f6689653817837995a31071187fa
Simon Cole <simonccole1@gmail.com>
2023-07-03 13:10:27 -0400

    number formatting

--- stringUtils.js
+++ stringUtils.js
@@ -3,4 +3,14 @@ lookup.toLowerCase() === item.toLowerCase());
 export const capitalize = (str) => {
     return str.length > 0 ? str.charAt(0).toUpperCase() + str.slice(1) : 
 str;
+}
+export const numberFormatter = (rawNumber) => {
+
+    var dec = String(rawNumber).split(/[.,]/),
+        sep = ',',
+        decsep = '.';
+
+    return dec[0].split('').reverse().reduce(function (prev, now, i) {
+        return i % 3 === 0 ? prev + sep + now : prev + now;
+    }).split('').reverse().join('') + (dec[1] ? decsep + dec[1] : '');
 }% 
